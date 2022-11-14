8445b60a9e9833ce0780e725099c91ec0d7e332c
John Montroy <jmontroy90@gmail.com>
2022-11-14 11:51:41 -0500

    docs(week1): add takeaways

--- week1/min_max_sum.go
+++ week1/min_max_sum.go
@@ -9,7 +9,7 @@ import (
 // Started at 11:18AM, finished at 11:29AM including tests. 11 minutes.
 // HackerRank seems to have a thing for int32. They'll be the same on most systems.
 // HackerRank also doesn't let you use `x` libraries. Changing to standard library.
-// RESULTS:
+// TAKEAWAYS:
 // * Okay, I missed overflow conditions. This wouldn't have happened if we just got to use int, since that would've been an int64 on my system.
 // * I also mixed up the sort direction on sort.Slice - if you preserve the index order, your sort will follow the operand, e.g. "<" -> smallest to largest
 // * Including playing with sorting and types, fully correct on HR at 11:38, so 20 minutes.
--- week1/plus_minus.go
+++ week1/plus_minus.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 )
 
-// Took about 20 minutes, most of it looking up different strconv functionality which I didn't actually need.
+// PlusMinus - took about 20 minutes, most of it looking up different strconv functionality which I didn't actually need.
 // Since they want formatted strings with 6 decimals of precision, we'll return strings here.
+// TAKEAWAYS:
+// * Spend some more time with the strconv library.
+// * Type conversions in general are annoying on these HackerRank questions.
 func PlusMinus(arr []int32) (pos string, neg string, zero string) {
 	var nPos, nNeg, nZero int
 	for _, v := range arr {
